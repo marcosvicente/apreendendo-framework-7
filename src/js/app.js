6f255defd02491d3173b6e66c8feda0fb19849c7
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2017-02-03 16:03:05 -0200

    Sidebar concluido

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
-var app = new Framework7;
-var $$ = Dom7;
+var app = new Framework7({
+  swipePanel: 'left',
+  debug: true
+});
 
+var $$ = Dom7
 
-// Abrir menu
-$$("open-panel").on('click', function(e) {
+/**
+ * Abrir menu
+ */
+$$("open-panel-right").on('click', function(e) {
   app.openPanel('right');
 });
