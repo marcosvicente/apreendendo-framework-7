5b7a6817eedd8dec2d74aa13e4ba691b84686f99
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2017-02-03 18:44:22 -0200

    Pagina filme criada

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,26 @@
 var app = new Framework7({
   swipePanel: 'left',
   debug: true
+
 });
 
 var $$ = Dom7
+
+
+var mainView = app.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+/**
+ * Page Filmes
+ */
+
+ // Callbacks to run specific code for specific pages, for example for About page:
+ app.onPageInit('filmes', function (page) {
+     
+ });
+
 
 /**
  * Abrir menu
